b5a6cc19630aee05dcec4979803275eb61193e12
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-08 15:10:33 +0300

    article test

---
+++ app/tests/articleTests.py
@@ -0,0 +1,26 @@
+import unittest
+from models import articles
+
+
+Article = articles.Articles
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Article('Football','https://media.istockphoto.com/photos/nairobi-downtown-capital-city-of-kenya-picture-id639495906?k=6&m=639495906&s=612x612&w=0&h=JBhS5G5WeI1LEChjGUuga_smUsW8adTmZ0TQ4JmTqN4=','Premier begins soon as the team prepare..','Fabrizio Romano', 'https://hackaday.com/2021/08/06/this-week-in-security-insecure-chargers-request-forgeries-and-kernel-security/', '12/8/2020')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article, Article))
+
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
