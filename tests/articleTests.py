455ff7b01780d77e82fb616d4759dfc5e87ac972
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-09 11:53:12 +0300

    test trals

--- tests/articleTests.py
+++ tests/articleTests.py
@@ -17,7 +17,7 @@ class ArticleTest(unittest.TestCase):
         self.new_article = Articles('Football','https://media.istockphoto.com/photos/nairobi-downtown-capital-city-of-kenya-picture-id639495906?k=6&m=639495906&s=612x612&w=0&h=JBhS5G5WeI1LEChjGUuga_smUsW8adTmZ0TQ4JmTqN4=','Premier begins soon as the team prepare..','Fabrizio Romano', 'https://hackaday.com/2021/08/06/this-week-in-security-insecure-chargers-request-forgeries-and-kernel-security/', '12/8/2020')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_article, Article))
+        self.assertTrue(isinstance(self.new_article, Articles))
 
 
 
